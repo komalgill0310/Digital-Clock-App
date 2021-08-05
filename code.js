8619a096219f58ec881bc3ff065a992f2a1c84cc
Komal Gill <komalgill0310@gmail.com>
2021-08-05 00:20:09 -0400

    if for getHours()-12

--- code.js
+++ code.js
@@ -10,11 +10,24 @@ function myTimer(){
   document.getElementById("time").innerHTML = time;  
 }
 
-var time = (today.getHours()-12) + ":" + today.getMinutes() + ":" + today.getSeconds(); 
+let h = today.getHours();
+let m = today.getMinutes();
+let s = today.getSeconds();
+
+if(h>12){
+  var time = (today.getHours()-12) + ":" + today.getMinutes() + ":" + today.getSeconds();
+}
+else{
+  var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); 
+}
+// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); 
 document.getElementById("time").innerHTML = time;
 
 var date = day[today.getDay()] + ',' + month[today.getMonth()] + today.getDate() + " " + today.getFullYear();
 document.getElementById("date").innerHTML = date;
+
+
+
 
 
 
