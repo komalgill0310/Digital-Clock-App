e52bb523b68939b66d79259a4302849e7931a18b
Komal Gill <komalgill0310@gmail.com>
2021-08-06 03:19:21 -0400

    js file is ready

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# Digital-Clock-App
-Clock is build using HTML, CSS and JavaScript.
+# Digital-Clock-App 
+<font size=6> Clock is build using HTML, CSS and JavaScript. </font>
 
-## To Open a Clock Project Click[here]()
+# To Open a Clock Project Click[here]  ()  
+
+# How does the website look
+<img src = "">
+
+# Summary
+<font size=5>This is the first project where I used JavaScript to give some functionality to my Digital Clock. In this application, I first created rough HTML and CSS and then I started working on that how can I get the local date and time. Later on, I started working on that how can I make the clock Tick. Also, in JS the getHours() methods shows the hour from 0-23H(military time clock) and I started working on that how can I convert the military clock to regular clock. Lastly, I made the time appear in HH:MM:SS. It was a great start towards learning JS. </font>
+
+# Author
+<font size=5>Komal - | Software Developer | [Digital Clock]() | [LinkedIn](www.linkedin.com/in/komalpreet-kaur-3b6924177)
+
+
+
 
--- code.js
+++ code.js
@@ -4,157 +4,27 @@ const month = ["Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","N
 
 setInterval(myTimer,1000);
 
-    // var m = today.getMinutes();
-    // var s = today.getSeconds();
-
-    function leadingZero(s){
-      if(s<10){
-        s = "0" + s;
+    function leadingZero(i){
+      if(i<10){
+        i = "0" + i;
       }
-    return s;
+    return i;
   }
-    // function leadingZero(m,s){
-    //   if(m,s<10){
-    //     m = "0" + today.getMinutes();
-    //     s = "0" + today.getSeconds();
-    //   }
-    //   return m,s;
-    // }
-
+    
 function myTimer(){    
     var today = new Date();  
-    // var h = today.getHours();
-    // var m = today.getMinutes();
-    var s = today.getSeconds();  
+    var hours = leadingZero(today.getHours());
+    var minutes = leadingZero(today.getMinutes());
+    var seconds = leadingZero(today.getSeconds());  
     if(today.getHours()>12){
-        var time = today.getHours()-12 + ":" + today.getMinutes() + ":" + s;
+        var time = hours-12 + ":" + minutes + ":" + seconds;
       }
       else{
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + s;
+        var time = hours + ":" + minutes + ":" + seconds;
       }
     document.getElementById("time").innerHTML = time;
     let date = day[today.getDay()] + ',' + month[today.getMonth()] + today.getDate() + " " + today.getFullYear();
     document.getElementById("date").innerHTML = date;  
 }
 
-    // function leadingZero(s){
-    //   // if(m,s<10){
-    //   //   m = "0" + today.getMinutes();
-    //   //   s = "0" + today.getSeconds();
-    //   // }
-    //   // return m,s;
-    //   return (today.getSeconds()<10)+"0"+today.getSeconds();
-     
-    // }
-
-
-
-    // m = leadingZero(m);
-    // s = leadingZero(s);
-
-    // m = leadingZero(m);
-    // s = leadingZero(s);
-// var i = m , s;
-
-
-
-
-// var  today = new Date();
-  // var m = today.getMinutes();
-  // var s = today.getSeconds();
-
-// function myTimer(){    
-//   let today = new Date();
-//   var h = today.getHours();
-//   var m = today.getMinutes();
-//   var s = today.getSeconds();
-
-//   function pad(m,2){
-//     m = today.getMinutes();
-//     while(today.getMinutes()<10){
-//       m = "0" + today.getMinutes();
-//     }
-//   }
-      
-//   function pad(h,size){
-//     h = h.toString();
-//     while(h.length<10){
-//       h = "0" + today.getHours();
-//     }
-//   }
-
-//   function pad(s,2){
-//     s = today.getSeconds();
-//     while(today.getSeconds()<10){
-//       s = "0" + today.getSeconds();
-//     }      
-//   }
-
-//   if(h>12){
-//       // var time = today.getHours()-12 + ":" + today.getMinutes() + ":" + today.getSeconds();
-//       var time = h-12 + ":" + m + ":" + s;
-//     }
-//     else{
-//       // var time = today.getHours() + ":" + m + ":" + today.getSeconds();
-//       var time = h + ":" + m + ":" + s;
-//     }
-//   document.getElementById("time").innerHTML = time;
-//   let date = day[today.getDay()] + ',' + month[today.getMonth()] + today.getDate() + " " + today.getFullYear();
-//   document.getElementById("date").innerHTML = date;
-
- 
   
-//   }
-
-// function pad(m,size){
-//   m = today.getMinutes();
-//   while(today.getMinutes()<10)
-//     m = "0" + today.getMinutes();
-// }
-
-
-// if(h>12){
-  //   var time = (today.getHours())-12 + ":" + today.getMinutes() + ":" + today.getSeconds();
-  // }
-  // else{
-  //   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); 
-  // }
-// let m = today.getMinutes();
-// let s = today.getSeconds();
-
-// if(h<10){
-//     var time = "0" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// }
-// else{
-//   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// }
-
-// if(m<10){
-//    var time =  today.getHours() + ":0" + today.getMinutes() + ":" + today.getSeconds();
-// }
-
-// if(s<10){
-//   var time =  (today.getHours()) + ":" + today.getMinutes() + ":0" + today.getSeconds();
-// }
-
-
-
-// else{
-//   var time = (today.getHours()) + ":" + today.getMinutes() + ":" + today.getSeconds();
-// }
-
-
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// while(h<10 | m<10 | s<10){
-//   var time = "0" + (today.getHours()-12) + ":0" + today.getMinutes() + ":0" + today.getSeconds();
-// }
-
-// if(h<10|m<10|s<10){
-//   var time = "0" + (today.getHours()-12) + ":" + today.getMinutes() + ":" + today.getSeconds();
-// }
-
-// if(h<10
-
-
-
-
